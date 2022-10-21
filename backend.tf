673d71b13cd595c3d7f1678f85f074d41c9078a8
Ahmed-Ali-Elbaz <ahmed.ali.elbaz.mohamed@gmail.com>
2022-10-21 20:48:18 +0200

    test branch

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "mybucket0362"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table  = "dynamodb-state-locking"  # to lock state file
-  }
-  
-}
